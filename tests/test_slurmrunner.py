1420697ab9c58a043d01f9d4c78881b875b81dba
Chris Churas <churas.camera@gmail.com>
2025-10-20 15:02:34 -0700

    fixed bug in tests

--- tests/test_slurmrunner.py
+++ tests/test_slurmrunner.py
@@ -8,6 +8,7 @@ import shutil
 import tempfile
 import unittest
 from dreutils.train import SLURMRunner
+from dreutils.exceptions import DreutilsError
 
 
 class TestSLURMRunner(unittest.TestCase):
@@ -30,10 +31,24 @@ class TestSLURMRunner(unittest.TestCase):
         self.assertEqual('hi', runner._outdir)
         self.assertEqual([], runner._algorithms)
 
+    def test_run_no_inputrocrates(self):
+        temp_dir = tempfile.mkdtemp()
+        try:
+            runner = SLURMRunner(outdir=temp_dir,
+
+                                 algorithms=['elastic_drecmd.py'])
+            res = runner.run()
+            self.fail('Expected exception')
+        except DreutilsError as de:
+            self.assertEqual('No input RO-Crates', str(de))
+        finally:
+            shutil.rmtree(temp_dir)
+
     def test_run(self):
         temp_dir = tempfile.mkdtemp()
         try:
             runner = SLURMRunner(outdir=temp_dir,
+                                 input_rocrates=['crate1'],
                                  algorithms=['elastic_drecmd.py'])
             res = runner.run()
             self.assertEqual(0, res)
